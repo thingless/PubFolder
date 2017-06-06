96aea594f5424e1846fffeffa0d4ba5a2b7ddcaa
Eric Swanson <eswanson@alloscomp.com>
2017-06-06 13:18:24 -0700

    Login works

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import tornado.curl_httpclient
 import tornado.ioloop
 import tornado.web
 from tornado.web import HTTPError
@@ -22,6 +23,11 @@ APP_KEY = os.environ.get('DBX_APP_KEY')
 APP_SECRET = os.environ.get('DBX_APP_SECRET')
 BASE_URL = os.environ.get('BASE_URL')
 
+USER_AGENT = 'PubFolder/1.0 (Python 3.6)'
+
+io_loop = tornado.ioloop.IOLoop.current()
+http_client = tornado.curl_httpclient.CurlAsyncHTTPClient(io_loop, max_clients=16)
+
 class PublicFolderHandler(tornado.web.RequestHandler):
     @gen.coroutine
     def get(self, uid, path):
@@ -35,15 +41,41 @@ class ListFolderHandler(tornado.web.RequestHandler):
 class LoginHandler(tornado.web.RequestHandler):
     @gen.coroutine
     def get(self):
-        print(self.request.uri)
         url = urllib.parse.urljoin(BASE_URL, '/login/continue')
-        self.redirect('https://www.dropbox.com/oauth2/authorize?client_id={client_id}&redirect_uri={redirect_uri}'.format(client_id=APP_KEY, redirect_uri=urllib.parse.quote(url)))
+        self.redirect('https://www.dropbox.com/oauth2/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code'.format(client_id=APP_KEY, redirect_uri=urllib.parse.quote(url)))
 
 class LoginContinueHandler(tornado.web.RequestHandler):
     @gen.coroutine
     def get(self):
-        print(self.request.uri)
-        self.write(self.request.uri)
+        code = self.get_argument('code')
+
+        url = 'https://api.dropboxapi.com/oauth2/token'
+        body = urllib.parse.urlencode({
+            'code': code,
+            'grant_type': 'authorization_code',
+            'redirect_uri': urllib.parse.urljoin(BASE_URL, '/login/continue'),
+        })
+        hreq = tornado.httpclient.HTTPRequest(url, method='POST', user_agent=USER_AGENT,
+                                              auth_username=APP_KEY, auth_password=APP_SECRET,
+                                              body=body, request_timeout=120)
+
+        try:
+            resp = yield http_client.fetch(hreq)
+        except Exception:
+            logger.exception("Exception when requesting %r", url)
+            raise HTTPError(500, 'Unable to obtain authorization code from Dropbox')
+
+        js = json.load(resp.buffer)
+
+        # Example JS:
+        # {
+        #     access_token: "-c0JMq0qLncAAAAAAABqMedBF0-_QbmwWHToG4jXXctB1qNBSxm0aZYhupbtC3PK",
+        #     token_type: "bearer",
+        #     uid: "113409",
+        #     account_id: "dbid:AABkoAh-HU3E5gJ2Ed1XR_Yait3IBadk0Ps"
+        # }
+
+        self.write(json.dumps(js))
 
 def make_app():
     return tornado.web.Application([
